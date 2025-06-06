6a8899b187f50b43e5e49e5bdb89f9e22727c14b
van-gomes <nninha.gomes@gmail.com>
2025-06-05 21:13:34 -0300

    fix: delete files

--- Program.cs
+++ Program.cs
@@ -49,8 +49,6 @@ namespace CSharpCourse
             
             //Métodos e Funções
             ExemploLambda.Executar();
-            LambdasDelegate.Executar();
-            UsandoDelegates.Executar();
         }
     }
 }
