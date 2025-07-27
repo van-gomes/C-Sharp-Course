ef65b6afa8917f1fe3073aaf5b5527894aa66186
van-gomes <nninha.gomes@gmail.com>
2025-07-27 18:50:00 -0300

    learn arithmetic operators

--- Fundamentos/LendoDados.cs
+++ Fundamentos/LendoDados.cs
@@ -1,6 +1,22 @@
-namespace CursoCSharp.Fundamentos;
+using System;
+using System.Globalization;
 
-public class LendoDados
+namespace CursoCSharp.Fundamentos
 {
-    
+    class LendoDados
+    {
+        public static void Executar() {
+            Console.Write("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Qual é a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual é o seu salário? ");
+            double salario = double.Parse(Console.ReadLine(),
+                CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome} {idade} R${salario}");
+        }
+    }
 }
--- Fundamentos/OperadorTernario.cs
+++ Fundamentos/OperadorTernario.cs
@@ -1,6 +1,17 @@
-namespace CursoCSharp.Fundamentos;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-public class OperadorTernario
+namespace CursoCSharp.Fundamentos
 {
-    
+    class OperadorTernario
+    {
+        public static void Executar() {
+            var nota = 9.0;
+            bool bomComportamento = true;
+            string resultado = nota >= 7.0 && bomComportamento 
+                ? "Aprovado" : "Reprovado";
+            Console.WriteLine(resultado);
+        }
+    }
 }
