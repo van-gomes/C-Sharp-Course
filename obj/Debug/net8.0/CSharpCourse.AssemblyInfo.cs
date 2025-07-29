23e51a6251643ef48cd40ab86b4740c9fba390ea
van-gomes <nninha.gomes@gmail.com>
2025-07-29 16:22:38 -0300

    make example unary operators

--- obj/Debug/net8.0/CSharpCourse.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharpCourse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpCourse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f013272ae30127f7b20cbd0ee621de18cd633dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa1e8a48752d2b78ad96e6077ad225a7bd0ce94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpCourse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpCourse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
